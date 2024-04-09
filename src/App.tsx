751dc8c4c68a696b57d0ca6c6a0070846b205dc7
Brandon Berisford <brandonberisford@gmail.com>
2024-04-09 08:59:24 -0500

    committing changes to work remote.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import { Viewer } from "resium"
+import { ArcGisBaseMapType, ArcGisMapServerImageryProvider, ArcGisMapService, Ion } from "cesium";
+import { useEffect } from "react";
+import { ImageryLayer, Viewer } from "resium";
 
 function App() {
+  const arcGisApiKey = import.meta.env.ARCGIS_API_KEY;
+  const cesiumAccessToken = import.meta.env.CESIUM_API_KEY;
+
+  Ion.defaultAccessToken = cesiumAccessToken;
+  ArcGisMapService.defaultAccessToken = arcGisApiKey;
+  let arcGisImagery: ArcGisMapServerImageryProvider;
+  let vfrImagery: ArcGisMapServerImageryProvider;
+
+  useEffect(() => {
+    async function loadImagery() {
+      arcGisImagery = await ArcGisMapServerImageryProvider.fromBasemapType(ArcGisBaseMapType.SATELLITE);
+      vfrImagery = await ArcGisMapServerImageryProvider.fromUrl("https://tiles.arcgis.com/tiles/ssFJjBXIUyZDrSYZ/arcgis/rest/services/VFR_Sectional/MapServer")
+    }
+  }, [])
+  
+  if(!arcGisImagery && !vfrImagery) return null
+
   return (
-      <Viewer full timeline={false} animation={false}/>
+    <Viewer
+    baseLayer={<ImageryLayer imageryProvider={arcGisImagery} />}
+    timeline={false}
+    animation={false}
+    geocoder={false}
+  >
+
+  </Viewer>
   )
 }
 
