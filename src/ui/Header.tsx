11ecec80671467e99577f299cf6a7543b1d61aa1
Brandon Berisford <brandonberisford@gmail.com>
2024-04-14 14:14:30 -0500

    making header better.

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -17,22 +17,24 @@ const Header: React.FC = () => {
   return (
     <header className="flex items-center justify-between px-4 py-2 bg-neutral text-neutral-content">
       <div className="flex items-center">
-        <button className="block mr-4 lg:hidden" onClick={() => dispatch(toggleSidebar())}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
-            />
-          </svg>
-        </button>
+        {isViewerPage && (
+          <button className="block mr-4 lg:hidden" onClick={() => dispatch(toggleSidebar())}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+              />
+            </svg>
+          </button>
+        )}
 
         {isViewerPage && <SearchBar />}
 
