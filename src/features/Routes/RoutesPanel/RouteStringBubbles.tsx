f046079a28aedd1ec3f35ab737f1e258246e72ed
Brandon Berisford <brandonberisford@gmail.com>
2024-05-10 20:19:33 -0500

    making route string bubbles wrap.

--- src/features/Routes/RoutesPanel/RouteStringBubbles.tsx
+++ src/features/Routes/RoutesPanel/RouteStringBubbles.tsx
@@ -8,8 +8,9 @@ interface RouteStringBubblesProps {
 const RouteStringBubbles: React.FC<RouteStringBubblesProps> = ({ routeString }) => {
   const codes = routeString.trim().split(' ');
 
+  if (!routeString) return <></>;
   return (
-    <div className="flex space-x-3">
+    <div className="flex flex-wrap gap-2">
       {codes.map((code, index) => (
         <RouteCodeBubble key={index} code={code} />
       ))}
