34cd360659b4c4039b117ae1a17fe602d18dc205
bberisford <brandonberisford@gmail.com>
2024-08-04 13:24:30 -0500

    fixing import statement.

--- src/redux/thunks/airports.ts
+++ src/redux/thunks/airports.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { airportsApi } from '../api/vfr3d/airportsSlice';
 import { AppState } from '../store';
+import { airportsApi } from '../api/vfr3d/airportsSlice.api';
 
 export const fetchAdditionalAirports = createAsyncThunk(
   'data/fetchAdditionalAirports',
@@ -8,7 +8,9 @@ export const fetchAdditionalAirports = createAsyncThunk(
     const state = getState() as AppState;
     const existingData = airportsApi.endpoints.getAllAirports.select(icaoIdOrArptId)(state);
     if (!existingData.isSuccess) {
-      const response = await dispatch(airportsApi.endpoints.getAllAirports.initiate(icaoIdOrArptId));
+      const response = await dispatch(
+        airportsApi.endpoints.getAllAirports.initiate(icaoIdOrArptId)
+      );
       return response.data;
     }
     return existingData;
